687acc9db7a1b26b359ccf5da1ec33f58c38738d
Yash Yadav <yashyadav.sr@gmail.com>
2024-06-20 20:16:29 +0400

    feat: can add shapes now

--- canva-apps-sdk-starter-kit/src/app.tsx
+++ canva-apps-sdk-starter-kit/src/app.tsx
@@ -1,27 +1,31 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
+import React from "react";
 import { addNativeElement } from "@canva/design";
-import * as React from "react";
-import styles from "styles/components.css";
 
-export const App = () => {
-  const onClick = () => {
-    addNativeElement({
-      type: "TEXT",
-      children: ["Hello world!"],
+export function App() {
+  async function handleClick() {
+    // Add the shape to the design
+    await addNativeElement({
+      type: "SHAPE",
+      paths: [
+        {
+          d: "M 0 0 H 100 V 100 H 0 L 0 0",
+          fill: {
+            color: "#ECD7A5",
+          },
+        },
+      ],
+      viewBox: {
+        height: 100,
+        width: 100,
+        left: 0,
+        top: 0,
+      },
     });
-  };
+  }
 
   return (
-    <div className={styles.scrollContainer}>
-      <Rows spacing="2u">
-        <Text>
-          To make changes to this app, edit the <code>src/app.tsx</code> file,
-          then close and reopen the app in the editor to preview the changes.
-        </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
-        </Button>
-      </Rows>
+    <div>
+      <button onClick={handleClick}>Add shape to design</button>
     </div>
   );
-};
+}
