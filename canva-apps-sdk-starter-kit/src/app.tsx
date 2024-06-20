9991cd3a3ec6c9464fb0be6123de0fd5f4bbc4d7
Yash Yadav <yashyadav.sr@gmail.com>
2024-06-20 22:26:35 +0400

    feat: minor adjustments

--- canva-apps-sdk-starter-kit/src/app.tsx
+++ canva-apps-sdk-starter-kit/src/app.tsx
@@ -6,16 +6,7 @@ export function App() {
   async function handleClick() {
     // Generate a random color
     const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    let pathString = `M50 50 \
-           C40 40, 30 20, 40 10 \
-           C50 0, 70 10, 60 20 \
-           C50 30, 60 40, 50 50 \
-           C60 40, 70 30, 80 40 \
-           C90 50, 80 70, 70 60 \
-           C60 50, 50 60, 50 50 \
-           C40 60, 30 70, 20 60 \
-           C10 50, 20 30, 30 40 \
-           C40 50, 50 40, 50 50 Z`;
+    let pathString = "M50 50 C20 20, 80 20, 50 50 L50 50 Z";
            
     // Add the shape to the design with the random color
     await addNativeElement({
