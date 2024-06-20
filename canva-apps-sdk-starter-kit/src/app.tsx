3dc5b2a505f86b6e99ede91243cdd1aca85b182e
Yash Yadav <yashyadav.sr@gmail.com>
2024-06-20 21:17:33 +0400

    feat: can add shapes now

--- canva-apps-sdk-starter-kit/src/app.tsx
+++ canva-apps-sdk-starter-kit/src/app.tsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { Button, Rows, Text, Title, FormField, NumberInput} from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
 
 export function App() {
   async function handleClick() {
-    // Add the shape to the design
+    // Generate a random color
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    // Add the shape to the design with the random color
     await addNativeElement({
       type: "SHAPE",
       paths: [
         {
-          d: "M 0 0 H 100 V 100 H 0 L 0 0",
+          d: "M50 50 \
+           C40 40, 30 20, 40 10 \
+           C50 0, 70 10, 60 20 \
+           C50 30, 60 40, 50 50 \
+           C60 40, 70 30, 80 40 \
+           C90 50, 80 70, 70 60 \
+           C60 50, 50 60, 50 50 \
+           C40 60, 30 70, 20 60 \
+           C10 50, 20 30, 30 40 \
+           C40 50, 50 40, 50 50 Z",
           fill: {
-            color: "#ECD7A5",
+            color: randomColor,
           },
         },
       ],
@@ -25,7 +38,17 @@ export function App() {
 
   return (
     <div>
-      <button onClick={handleClick}>Add shape to design</button>
+      <Rows spacing="2u">
+      <Rows spacing="1u">
+        <Text> </Text>
+        <Title>Yash's flower generator!</Title>
+        <Text> Hello! This is a cool new flower generator machine that generates new flower shapes for you!</Text>
+      </Rows>
+      <FormField label="Number of petals" description="It takes the number of petals." control={(props) => (
+        <NumberInput {...props} />
+      )} />
+      <Button variant="primary" onClick={handleClick} stretch>Add shape to design</Button>
+      </Rows>
     </div>
   );
 }
